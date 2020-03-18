681493bee7bb8c6b84e3587d270240c4a579d542
Luan <luan.nascimento11@outlook.com>
2020-03-17 22:37:16 -0300

    Configração da template engine

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4dc4da0fae2da2',
+    pass: '989505d525988a',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
